f2c7653ee61827688777cccc368027cc19c97a43
sukku-tech <cherukupallisukanya808gmail.com>
2024-09-15 19:59:18 +0530

    collecting-user-details

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,38 @@
-import React from "react"
+import React,{useState} from "react"
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css"
 import Button from "../UI/Button";
 
+
 const AddUser=()=>{
+    const [latestName,setLatestName]=useState("");
+    const [latestAge,setLatestAge]=useState("");
+
+    const nameChangeHandler=(event)=>{
+        setLatestName(event.target.value);
+
+    }
+    const ageChangeHandler=(event)=>{
+        setLatestAge(event.target.value);
+
+    }
 
   const addUserHandler=(event)=>{
     event.preventDefault();
+    const userDetails={
+        name:latestName,
+        age:latestAge
+    }
+    console.log(userDetails);
   }
+
   return (
     <Card>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username" className={classes.label}>Username</label>
-        <input id="username" type="text" className={classes.input}/>
+        <input id="username" type="text" className={classes.input} onChange={nameChangeHandler}/>
         <label htmlFor="age" className={classes.label}>Age</label>
-        <input id="age" type="number" className={classes.input}/>
+        <input id="age" type="number" className={classes.input} onChange={ageChangeHandler}/>
         <Button type="submit">Add User</Button>
         
       </form>
